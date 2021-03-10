9320aa0c77166d33f02f8cc4d1e9d4dd533942d5
gwyneplaine <cc.lee@live.com.au>
2021-03-10 16:24:16 +1100

    add a file system cache ffor caching retrieved tutorial data from the monorepo

--- docs-next/lib/tutorials.ts
+++ docs-next/lib/tutorials.ts
@@ -5,32 +5,50 @@ import renderToString from 'next-mdx-remote/render-to-string';
 import { components } from '../components/Page';
 import matter from 'gray-matter';
 
-const packageMap = new Map();
-
 export async function getTutorialData(slug) {
-  if (packageMap.has(slug)) {
-    const { content, data } = packageMap.get(slug);
+  try {
+    const cache = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), '.cache'), 'utf-8'));
+    console.log(cache);
+    const { content, data } = cache[slug];
+    console.log(content, data);
     const children = await renderToString(content, { components, scope: data });
-
+    console.log(children);
     return { children, data };
+  } catch (e) {
+    return null;
   }
+}
+
+function cacheToDisk(packages) {
+  const cache = {};
+  for (const pkg of packages) {
+    const markdown = fs.readFileSync(path.resolve(pkg.dir, 'docs', 'index.mdx'), 'utf-8');
+    const slug = pkg.packageJson.name.replace('@keystone-tutorials/', '');
+    const { content, data } = matter(markdown);
+    cache[slug] = { content, data };
+  }
+
+  const writePath = path.resolve(process.cwd(), '.cache');
+  fs.writeFileSync(writePath, JSON.stringify(cache, null, 2), {
+    encoding: 'utf-8',
+  });
 }
 
 export async function getTutorialIds() {
   const tutorialIds = await getPackages(process.cwd()).then(({ root, packages }) => {
-    return packages
-      .filter(pkg => pkg.dir.includes(path.resolve(root.dir, 'tutorials')))
-      .map(pkg => {
-        const markdown = fs.readFileSync(path.resolve(pkg.dir, 'docs', 'index.mdx'), 'utf-8');
-        const slug = pkg.packageJson.name.replace('@keystone-tutorials/', '');
-        const { content, data } = matter(markdown);
-        packageMap.set(slug, { content, data });
-        return {
-          params: {
-            slug: pkg.packageJson.name.replace('@keystone-tutorials/', ''),
-          },
-        };
-      });
+    const relevantPackages = packages.filter(pkg =>
+      pkg.dir.includes(path.resolve(root.dir, 'tutorials'))
+    );
+
+    cacheToDisk(relevantPackages);
+
+    return relevantPackages.map(pkg => {
+      return {
+        params: {
+          slug: pkg.packageJson.name.replace('@keystone-tutorials/', ''),
+        },
+      };
+    });
   });
 
   return tutorialIds;
--- docs-next/pages/tutorials/[slug].tsx
+++ docs-next/pages/tutorials/[slug].tsx
@@ -1,12 +1,12 @@
 /** @jsx jsx */
 import { jsx } from '@keystone-ui/core';
 import { getTutorialIds, getTutorialData } from '../../lib/tutorials';
-import { Page, components } from '../../components/Page';
+import { Page } from '../../components/Page';
 import { hydrate } from '../../components/_utils';
 
 export async function getStaticProps({ params: { slug } }) {
   const { children, data } = await getTutorialData(slug);
-
+  console.log('WHAT TEH FUCK', children, data);
   return { props: { children, data } };
 }
 
