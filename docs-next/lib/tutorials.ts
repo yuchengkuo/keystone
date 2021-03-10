b9334b82df4e4a324c7537cd4b689be474fe9e82
gwyneplaine <cc.lee@live.com.au>
2021-03-10 16:23:33 +1100

    add .cache to gitignore

--- docs-next/lib/tutorials.ts
+++ docs-next/lib/tutorials.ts
@@ -5,28 +5,32 @@ import renderToString from 'next-mdx-remote/render-to-string';
 import { components } from '../components/Page';
 import matter from 'gray-matter';
 
-export async function getTutorialData(id) {
-  const tutorialDir = await getPackages(process.cwd()).then(({ packages }) => {
-    return packages.find(pkg => pkg.packageJson.name === `@keystone-tutorials/${id}`).dir;
-  });
+const packageMap = new Map();
 
-  const markdown = fs.readFileSync(path.resolve(tutorialDir, 'docs', 'index.mdx'), 'utf-8');
+export async function getTutorialData(slug) {
+  if (packageMap.has(slug)) {
+    const { content, data } = packageMap.get(slug);
+    const children = await renderToString(content, { components, scope: data });
 
-  const { content, data } = matter(markdown);
-  const children = await renderToString(content, { components, scope: data });
-
-  return { children, data };
+    return { children, data };
+  }
 }
 
 export async function getTutorialIds() {
   const tutorialIds = await getPackages(process.cwd()).then(({ root, packages }) => {
     return packages
       .filter(pkg => pkg.dir.includes(path.resolve(root.dir, 'tutorials')))
-      .map(pkg => ({
-        params: {
-          slug: pkg.packageJson.name.replace('@keystone-tutorials/', ''),
-        },
-      }));
+      .map(pkg => {
+        const markdown = fs.readFileSync(path.resolve(pkg.dir, 'docs', 'index.mdx'), 'utf-8');
+        const slug = pkg.packageJson.name.replace('@keystone-tutorials/', '');
+        const { content, data } = matter(markdown);
+        packageMap.set(slug, { content, data });
+        return {
+          params: {
+            slug: pkg.packageJson.name.replace('@keystone-tutorials/', ''),
+          },
+        };
+      });
   });
 
   return tutorialIds;
