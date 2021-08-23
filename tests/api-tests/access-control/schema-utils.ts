191b03a857d571c0f68f5c36b4768e8331cae3a4
Tim Leslie <timl@thinkmill.com.au>
2021-08-23 10:21:33 +1000

    WIP

--- tests/api-tests/access-control/schema-utils.ts
+++ tests/api-tests/access-control/schema-utils.ts
@@ -1,5 +1,5 @@
 import { text } from '@keystone-next/keystone/fields';
-import { createSchema, list } from '@keystone-next/keystone/schema';
+import { createSchema, list } from '@keystone-next/keystone';
 import { statelessSessions } from '@keystone-next/keystone/session';
 import { apiTestConfig } from '../utils';
 
